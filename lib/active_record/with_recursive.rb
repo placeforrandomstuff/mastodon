6f058d4fe0c898d6ec7fa192fedd98ad4463e150
Claire <claire.github-309c@sitedethib.com>
2024-04-18 15:11:03 +0200

    Change `with_recursive` into `with.recursive`

--- lib/active_record/with_recursive.rb
+++ lib/active_record/with_recursive.rb
@@ -2,14 +2,45 @@
 
 # Add support for writing recursive CTEs in ActiveRecord
 
-# From Lorin Thwaits (https://github.com/lorint) as per comment:
+# Initially from Lorin Thwaits (https://github.com/lorint) as per comment:
 # https://github.com/vlado/activerecord-cte/issues/16#issuecomment-1433043310
+
+# Modified from the above code to change the signature to
+# `with.recursive(hash)` and extending CTE hash values to also includes arrays
+# of values that get turned into UNION ALL expressions.
 
 module ActiveRecord
   module QueryMethodsExtensions
-    def with_recursive(*args)
-      @is_recursive = true
-      with(*args)
+    class WithChain
+      def initialize(scope) # :nodoc:
+        @scope = scope
+      end
+
+      def recursive(*args)
+        @scope.with_values += args
+        @scope.is_recursive = true
+        @scope
+      end
+    end
+
+    # TODO: This is akin to how `Relation::VALUE_METHODS` are defined,
+    # but this does not neatly fit into one of the existing categories.
+    # Maybe we should make a full-fledged `WithClause`
+    def is_recursive # rubocop:disable Naming/PredicateName
+      @values.fetch(:is_recursive, false)
+    end
+
+    def is_recursive=(value)
+      assert_mutability!
+      @values[:is_recursive] = value
+    end
+
+    def with(*args)
+      if args.empty?
+        WithChain.new(spawn)
+      else
+        spawn.with!(*args)
+      end
     end
 
     private
@@ -24,7 +55,7 @@ module ActiveRecord
       end
 
       # Was:  arel.with(with_statements)
-      @is_recursive ? arel.with(:recursive, with_statements) : arel.with(with_statements)
+      is_recursive ? arel.with(:recursive, with_statements) : arel.with(with_statements)
     end
 
     def build_with_value_from_hash(hash)
